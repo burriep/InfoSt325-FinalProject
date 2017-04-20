c37930930b7f4df312cd0ef9a7c06c4156fbd7ad
zachzez <zezgamer@gmail.com>
2017-04-19 20:25:18 -0500

    Update BasicCrypt.java

--- src/edu/uwm/infost325/BasicCrypt.java
+++ src/edu/uwm/infost325/BasicCrypt.java
@@ -3,6 +3,11 @@ package edu.uwm.infost325;
  * Final InfoSt 325 Project
  */
 public class BasicCrypt {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+            JFrame frame = new BasicCryptGUI();
+            frame.setTitle("Cryptology");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);  		
 	}
 }
